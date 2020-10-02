5ea82b9d065b3e2376f4aa25dd85ab2bfb5462b1
biagavirete <biagavirete@gmail.com>
2020-10-02 14:56:59 -0300

    add user id to appointments

---
+++ src/shared/infra/typeorm/migrations/1601659740575-AddUserIdToAppointments.ts
@@ -0,0 +1,41 @@
+import {
+    MigrationInterface,
+    QueryRunner,
+    TableColumn,
+    TableForeignKey,
+} from 'typeorm';
+
+export default class AddUserIdToAppointments1601659740575
+    implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.addColumn(
+            'appointments',
+            new TableColumn({
+                name: 'user_id',
+                type: 'uuid',
+                isNullable: true,
+            }),
+        );
+
+        await queryRunner.createForeignKey(
+            'appointments',
+            new TableForeignKey({
+                name: 'AppointmentUser',
+                columnNames: ['user_id'],
+                referencedColumnNames: ['id'],
+                referencedTableName: 'users',
+                onDelete: 'SET NULL',
+                onUpdate: 'CASCADE',
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey(
+            'appointments',
+            'AppointmentsProvider',
+        );
+
+        await queryRunner.dropColumn('appointments', 'user_id');
+    }
+}
