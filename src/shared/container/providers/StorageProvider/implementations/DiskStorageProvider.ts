d5d17c245017b10a7b2949e27b75e4cca9e87268
biagavirete <biagavirete@gmail.com>
2020-10-29 15:20:16 -0300

    fix providers info

--- src/shared/container/providers/StorageProvider/implementations/DiskStorageProvider.ts
+++ src/shared/container/providers/StorageProvider/implementations/DiskStorageProvider.ts
@@ -7,14 +7,14 @@ class DiskStorageProvider implements IStorageProvider {
     public async saveFile(file: string): Promise<string> {
         await fs.promises.rename(
             path.resolve(uploadConfig.tmpFolder, file),
-            path.resolve(uploadConfig.uploadFolder, file),
+            path.resolve(uploadConfig.uploadsFolder, file),
         );
 
         return file;
     }
 
     public async deleteFile(file: string): Promise<void> {
-        const filePath = path.resolve(uploadConfig.uploadFolder, file);
+        const filePath = path.resolve(uploadConfig.uploadsFolder, file);
 
         try {
             await fs.promises.stat(filePath);
